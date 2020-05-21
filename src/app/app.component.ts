d03b53bbfc96e2358f84d62c5ab9113c70ecd7b1
phil-trinh <philip.trinh@gsa.gov>
2020-05-21 14:32:25 -0400

    Enable and fix popover behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+// Declare jQuery symbol
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gear3';
+
+  ngOnInit(){
+    // Enable all popovers
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    })
+  }
 }
