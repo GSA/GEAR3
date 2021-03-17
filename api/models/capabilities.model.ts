3688ea1fdd0b001d5e91f63d2cc7665011c17a20
phil-trinh <philip.trinh@gsa.gov>
2021-03-17 14:08:38 -0400

    Update capabilities model

--- api/models/capabilities.model.ts
+++ api/models/capabilities.model.ts
@@ -1,8 +1,8 @@
 export class Capability {
   public ID: number;
   public Name: string;
-  public Description: string;
   public ReferenceNum: string;
+  public Description: string;
+  public Level: string;
   public Parent: string;
-  public old_Id: number;
 }
