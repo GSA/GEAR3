2a71a2fa30287a646f1cdf4905ff3aabf6a9787f
phil-trinh <philip.trinh@gsa.gov>
2021-03-18 15:51:28 -0400

    Comment related capabilities components to systems until that section is built out

--- api/controllers/capabilities.controller.js
+++ api/controllers/capabilities.controller.js
@@ -14,22 +14,22 @@ exports.findAll = (req, res) => {
 
 exports.findOne = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_capabilities.sql')).toString() +
-    ` WHERE cap.Id = ${req.params.id};`;
+    ` WHERE cap.capability_Id = ${req.params.id};`;
 
   res = ctrl.sendQuery(query, 'individual business capability', res);
 };
 
-exports.findApplications = (req, res) => {
-  var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_applications.sql')).toString() +
-    ` AND cap.Id = ${req.params.id} GROUP BY app.Id;`;
+// exports.findApplications = (req, res) => {
+//   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_applications.sql')).toString() +
+//     ` AND cap.capability_Id = ${req.params.id} GROUP BY app.Id;`;
 
-  res = ctrl.sendQuery(query, 'supporting applications for capability', res);
-};
+//   res = ctrl.sendQuery(query, 'supporting applications for capability', res);
+// };
 
-exports.findSSO = (req, res) => {
-  var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_capabilities_by_org.sql')).toString() +
-    ` WHERE org.Display_name LIKE '%${req.params.name}%' AND cap.ReferenceNumber IS NOT NULL AND appStatus.Keyname <> 'Retired'
-    GROUP BY cap.Id;`;
+// exports.findSSO = (req, res) => {
+//   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_capabilities_by_org.sql')).toString() +
+//     ` WHERE org.Display_name LIKE '%${req.params.name}%' AND cap.ReferenceNumber IS NOT NULL AND appStatus.Keyname <> 'Retired'
+//     GROUP BY cap.capability_Id;`;
 
-  res = ctrl.sendQuery(query, 'capabilities for specified SSO', res);
-};
+//   res = ctrl.sendQuery(query, 'capabilities for specified SSO', res);
+// };
