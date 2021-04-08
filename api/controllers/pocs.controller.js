8ca35b2a7008df2da44f639a2c1fe3b0a3e3dc0c
phil-trinh <philip.trinh@gsa.gov>
2021-04-08 15:02:35 -0400

    Change POC API to refer to old database

--- api/controllers/pocs.controller.js
+++ api/controllers/pocs.controller.js
@@ -9,19 +9,19 @@ exports.findAll = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_pocs.sql')).toString() +
     " ORDER BY poc.Keyname;";
 
-  res = ctrl.sendQuery(query, 'Points of Contact', res);
+  res = ctrl.sendQuery_cowboy(query, 'Points of Contact', res);
 };
 
 exports.findOne = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_pocs.sql')).toString() +
     ` WHERE poc.Id = ${req.params.id};`;
 
-  res = ctrl.sendQuery(query, 'individual POC', res);
+  res = ctrl.sendQuery_cowboy(query, 'individual POC', res);
 };
 
 exports.findRissos = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_pocs.sql')).toString() +
     " WHERE poc.RISSO <> 24;";
 
-  res = ctrl.sendQuery(query, 'RISSO POCs', res);
+  res = ctrl.sendQuery_cowboy(query, 'RISSO POCs', res);
 };
