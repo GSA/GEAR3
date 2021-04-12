695366632c6b159a27c79f49bf3f9133957f3008
phil-trinh <philip.trinh@gsa.gov>
2021-04-12 16:53:06 -0400

    Switch Org API to old database

--- api/controllers/organizations.controller.js
+++ api/controllers/organizations.controller.js
@@ -9,19 +9,19 @@ exports.findAll = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_organizations.sql')).toString() +
     " ORDER BY Name;";
 
-  res = ctrl.sendQuery(query, 'organizations', res);
+  res = ctrl.sendQuery_cowboy(query, 'organizations', res);
 };
 
 exports.findOne = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_organizations.sql')).toString() +
     ` WHERE org.Id = ${req.params.id};`;
 
-  res = ctrl.sendQuery(query, 'individual organization', res);
+  res = ctrl.sendQuery_cowboy(query, 'individual organization', res);
 };
 
 exports.findApplications = (req, res) => {
   var query = fs.readFileSync(path.join(__dirname, queryPath, 'GET/get_applications.sql')).toString() +
     ` AND owner.Id = ${req.params.id} GROUP BY app.Id;`;
 
-  res = ctrl.sendQuery(query, 'applications for organization', res);
+  res = ctrl.sendQuery_cowboy(query, 'applications for organization', res);
 };
