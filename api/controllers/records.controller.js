e9ffec07205cac9fb2845a24f0cd41dfa00d95e4
phil-trinh <philip.trinh@gsa.gov>
2021-05-28 14:11:58 -0400

    Add Records Management API; Refactor Google API access for both records and system TIME APIs

--- api/controllers/records.controller.js
+++ api/controllers/records.controller.js
@@ -1,9 +1,9 @@
 const ctrl      = require('./base.controller'),
 
-      SHEET_ID = '1_9X39tQ6jbYARcEYF7KbO-lDGbpDPjIwtKx0qhFvohE',
-      RANGE = 'MASTER!A:M';
+      SHEET_ID = '1eSoyn7-2EZMqbohzTMNDcFmNBbkl-CzXtpwNXHNHD1A',
+      RANGE = 'Master List';
 
-// @see https://docs.google.com/spreadsheets/d/1_9X39tQ6jbYARcEYF7KbO-lDGbpDPjIwtKx0qhFvohE
+// @see https://docs.google.com/spreadsheets/d/1eSoyn7-2EZMqbohzTMNDcFmNBbkl-CzXtpwNXHNHD1A
 
 exports.findAll = (req, res) => {
   ctrl.googleMain(res, 'all', SHEET_ID, RANGE);
