bff7ac418ff6fa77503851aa39f6813c86011159
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2023-03-20 15:18:48 -0400

    #120 added decommissioned filter as well as table initialization filters.

--- src/app/views/systems/websites/websites.component.ts
+++ src/app/views/systems/websites/websites.component.ts
@@ -17,6 +17,8 @@ declare var $: any;
   styleUrls: ['./websites.component.css'],
 })
 export class WebsitesComponent implements OnInit {
+  filteredTable: boolean = false;
+
   constructor(
     private apiService: ApiService,
     private location: Location,
@@ -46,75 +48,92 @@ export class WebsitesComponent implements OnInit {
   });
 
   // Apps Table Columns
-  columnDefs: any[] = [{
-    field: 'domain',
-    title: 'Domain',
-    sortable: true
-  }, {
-    field: 'office',
-    title: 'Office',
-    sortable: true
-  }, {
-    field: 'site_owner_email',
-    title: 'Site Owner',
-    sortable: true
-  }, {
-	field: 'contact_email',
-    title: 'Contact Email',
-    sortable: true
-  }, {
-	field: 'production_status',
-    title: 'Status',
-    sortable: true
-  }, {
-	field: 'redirects_to',
-    title: 'Redirect URL',
-    sortable: true
-  }, {
-	field: 'required_by_law_or_policy',
-    title: 'Required?',
-    sortable: true
-  }, {
-	field: 'has_dap',
-    title: 'Uses DAP?',
-    sortable: true
-  }, {
-	field: 'mobile_friendly',
-    title: 'Mobile Friendly?',
-    sortable: true
-  }, {
-	field: 'has_search',
-    title: 'Has Search?',
-    sortable: true
-  }, {
-	field: 'repository_url',
-    title: 'Repository URL',
-    sortable: true
-  }, {
-	field: 'hosting_platform',
-    title: 'Hosting Platform',
-    sortable: true
-  }, {
-	field: 'cms_platform',
-    title: 'Content Management Platform',
-    sortable: true
-  }, {
-	field: 'https',
-    title: 'HTTPS?',
-    sortable: true
-  }, {
-  field: 'sub_office',
-    title: 'Sub-office',
-    sortable: false,
-    visible: false,
-    class: 'text-truncate'
-  }, {
-    field: 'type_of_site',
-    title: 'Type of Site',
-    sortable: false,
-    visible: false,
-    class: 'text-truncate'
-  }];
+  columnDefs: any[] = [
+    {
+      field: 'domain',
+      title: 'Domain',
+      sortable: true,
+    },
+    {
+      field: 'office',
+      title: 'Office',
+      sortable: true,
+    },
+    {
+      field: 'site_owner_email',
+      title: 'Site Owner',
+      sortable: true,
+    },
+    {
+      field: 'contact_email',
+      title: 'Contact Email',
+      sortable: true,
+    },
+    {
+      field: 'production_status',
+      title: 'Status',
+      sortable: true,
+    },
+    {
+      field: 'redirects_to',
+      title: 'Redirect URL',
+      sortable: true,
+    },
+    {
+      field: 'required_by_law_or_policy',
+      title: 'Required?',
+      sortable: true,
+    },
+    {
+      field: 'has_dap',
+      title: 'Uses DAP?',
+      sortable: true,
+    },
+    {
+      field: 'mobile_friendly',
+      title: 'Mobile Friendly?',
+      sortable: true,
+    },
+    {
+      field: 'has_search',
+      title: 'Has Search?',
+      sortable: true,
+    },
+    {
+      field: 'repository_url',
+      title: 'Repository URL',
+      sortable: true,
+    },
+    {
+      field: 'hosting_platform',
+      title: 'Hosting Platform',
+      sortable: true,
+    },
+    {
+      field: 'cms_platform',
+      title: 'Content Management Platform',
+      sortable: true,
+    },
+    {
+      field: 'https',
+      title: 'HTTPS?',
+      sortable: true,
+    },
+    {
+      field: 'sub_office',
+      title: 'Sub-office',
+      sortable: false,
+      visible: false,
+      class: 'text-truncate',
+    },
+    {
+      field: 'type_of_site',
+      title: 'Type of Site',
+      sortable: false,
+      visible: false,
+      class: 'text-truncate',
+    },
+  ];
 
   ngOnInit(): void {
     // Enable popovers
@@ -131,6 +150,9 @@ export class WebsitesComponent implements OnInit {
         data: [],
       })
     );
+
+    // Sets initial filtering of table
+    $(document).ready(this.resetTableFilters());
 
     // Method to handle click events on the Website table
     $(document).ready(
@@ -156,6 +178,24 @@ export class WebsitesComponent implements OnInit {
             this.tableService.websitesTableClick(data[0]);
           });
       }
+    });
+  }
+
+  //The following is adapted from fisma.component.ts to filter on multiple columns of data rather than one
+  // Update table to Cloud Business Systems
+  showDecommissioned() {
+    this.filteredTable = true; // Expose main table button after "Cloud Enabled" button is pressed
+
+    // Filter to only "Cloud" Business Systems/Subsystems
+    $('#websitesTable').bootstrapTable('filterBy', {
+      Status: ['decommissioned'],
+    });
+  }
+
+  resetTableFilters() {
+    $('#websitesTable').bootstrapTable('filterBy', {
+      Status: ['production', 'archived'],
+      type_of_site: ['informational', 'application', 'application login'],
     });
   }
 }
