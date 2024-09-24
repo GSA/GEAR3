f8ce99d6f7858176bad826e29d21b1ed695152bb
khgsa <161092171+khgsa@users.noreply.github.com>
2024-09-24 06:18:03 -0700

    node-cron dependency update

--- server.ts
+++ server.ts
@@ -393,8 +393,15 @@ const port = process.env['PORT'] || 4000;
     console.log('Listening on ' + bind);
   }
 }
-
-run();
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
 
 /*
  * Function to get FISMA info from ServiceNow API
